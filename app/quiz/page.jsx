8ccddee4afb865995a387538d983709cbba6e486
Yazdun <yazduntube@gmail.com>
2023-05-12 20:19:16 +0430

    commit

---
+++ app/quiz/page.jsx
@@ -0,0 +1,9 @@
+import { Container } from '@/components'
+
+export default async function Page() {
+  return (
+    <main>
+      <Container className="py-5">Quiz Page</Container>
+    </main>
+  )
+}
--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,13 +1,21 @@
 import Link from 'next/link'
 import { Container } from '.'
 import Image from 'next/image'
+import { TbArrowBigRightFilled } from 'react-icons/tb'
 
 export const Navigation = () => {
   return (
     <div className="sticky top-0 backdrop-blur-xl bg-[rgba(0,0,0,0.8)] border-b border-slate-800 z-50">
-      <Container className="py-5">
+      <Container className="flex justify-between py-5">
         <Link href="/">
           <Image src="/logo.png" alt="Family Guy" width={70} height={50} />
+        </Link>
+        <Link
+          href="/quiz"
+          className="flex items-center justify-center gap-1 px-5 font-semibold text-black transition-colors bg-green-500 rounded-md duration-600 hover:bg-green-600"
+        >
+          <TbArrowBigRightFilled className="text-lg" />
+          Take a Quiz
         </Link>
       </Container>
     </div>
