34d938bcda69e1e978b6a2c9512cf3ed9c25c62b
Yazdun <yazduntube@gmail.com>
2023-05-24 19:37:20 +0430

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -4,31 +4,14 @@ Renders a Next.js page component that displays a grid of character avatars with 
 @component
 @returns {JSX.Element} The rendered page component.
 */
+
 import { Container } from '@/components'
 import Image from 'next/image'
 import Link from 'next/link'
-import { endpoint } from '@/utils/endpoint'
-/**
-
-Retrieves data from the specified endpoint path.
-@async
-@param {string} path - The endpoint path to fetch data from.
-@returns {Promise<Object>} A promise that resolves to the fetched data.
-@throws {Error} If the data fetching fails.
-*/
-
-export async function getData(path) {
-  const data = await fetch(`${endpoint}/${path}`)
-
-  if (!data.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  return data.json()
-}
+import { getAllCharacters } from '@/lib/characters'
 
 export default async function Page() {
-  const data = await getData('/characters')
+  const data = await getAllCharacters()
 
   return (
     <main>
