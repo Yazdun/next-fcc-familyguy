d31d2f98b6a84528fa245d3b440264b368076377
Devendra Dhakad <dkdhakad97@gmail.com>
2023-12-28 17:55:36 +0530

    Update page.jsx

--- app/quiz/[id]/page.jsx
+++ app/quiz/[id]/page.jsx
@@ -17,6 +17,13 @@ export default async function Page({ params }) {
   return (
     <Container as="main" className="flex flex-col gap-5 py-5">
       <h1 className="text-lg font-semibold">{question.title}</h1>
+      {question.quiz_image ? <Image 
+                  src={question.quiz_image}
+                  alt=""
+                  className='transition-all duration-500 hover:scale-110 hover:-rotate-2'
+                  width={300}
+                  height={300}
+                /> : null}
       <Answer answers={question.answers} questionId={params.id} />
     </Container>
   )
